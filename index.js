2c7d1f5173d891d2fc3b6787c55781d8f713c045
camilawatanabe1908 <camilawatanabe@grupointegrado.br>
2026-03-09 21:05:32 -0300

    insercao do promptsync 2

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 console.log("Hello, World!");
 const prompt = require "prompt-sync"();
 let nome = prompt ("Por favor digite seu nome>");
-console.log("O nome digitado foi"+nome;
-    
+console.log("O nome digitado foi"+nome);
+
 
